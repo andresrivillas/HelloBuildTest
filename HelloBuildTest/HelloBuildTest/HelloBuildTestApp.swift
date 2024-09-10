3e6552a8110997ecfca13995757145976cf2528d
Andres Rivillas <arivillas@enlace.org>
2024-09-10 12:39:44 -0500

    First Commit

---
+++ HelloBuildTest/HelloBuildTest/HelloBuildTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloBuildTestApp.swift
+//  HelloBuildTest
+//
+//  Created by Andres Rivillas on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloBuildTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
